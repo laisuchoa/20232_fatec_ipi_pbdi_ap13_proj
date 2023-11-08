8120184557027269401625da58a889ce2379f9eb
Ronaldo Souza Chaves <ronaldosouzachaves@outlook.com>
2023-11-08 18:15:52 -0300

    docs(ap13): adiciona enunciados

---
+++ script.sql
@@ -0,0 +1,38 @@
+-- 1.1 Estude e faça o download da base de dados disponível no link https://www.kaggle.com/datasets/csafrit2/higher-education-students-performance-evaluation
+
+
+
+-- 1.2 Crie uma tabela apropriada para o armazenamento dos itens. Não se preocupe com a normalização. Uma tabela basta.
+
+
+
+-- 1.3 Copie os dados do arquivo .csv para a sua tabela. Veja como no link https://www.postgresql.org/docs/current/sql-copy.html
+
+
+
+-- 1.4 Escreva os seguintes stored procedures (incluindo um bloco anônimo de teste para cada um):
+
+-- 1.4.1 Exibe o número de estudantes maiores de idade.
+
+
+
+-- 1.4.2 Exibe o percentual de estudantes de cada sexo.
+
+
+
+-- 1.4.3 Recebe um sexo como parâmetro em modo IN e utiliza oito parâmetros em modo OUT para dizer qual o percentual de cada nota (variável grade) obtida por estudantes daquele sexo.
+
+
+
+-- 1.5 Escreva as seguintes functions (incluindo um bloco anônimo de teste para cada uma):
+
+-- 1.5.1 Responde (devolve boolean) se é verdade que todos os estudantes de renda acima de 410 são aprovados (grade > 0).
+
+
+
+-- 1.5.2 Responde (devolve boolean) se é verdade que, entre os estudantes que fazem anotações pelo menos algumas vezes durante as aulas, pelo menos 70% são aprovados (grade > 0).
+
+
+
+-- 1.5.3 Devolve o percentual de alunos que se preparam pelo menos um pouco para os “midterm exams” e que são aprovados (grade > 0).
+
