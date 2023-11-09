c30487e13bdfc0e1d6fae5d26895fe4223e10291
Ronaldo Souza Chaves <ronaldosouzachaves@outlook.com>
2023-11-08 21:27:48 -0300

    feat(ap13): adiciona 1.4.3

--- script.sql
+++ script.sql
@@ -81,7 +81,66 @@ $$
 
 -- 1.4.3 Recebe um sexo como parâmetro em modo IN e utiliza oito parâmetros em modo OUT para dizer qual o percentual de cada nota (variável grade) obtida por estudantes daquele sexo.
 
+CREATE OR REPLACE PROCEDURE percentualNotasGenero(IN genero INT, OUT fail NUMERIC, OUT dd NUMERIC, OUT dc NUMERIC, OUT cc NUMERIC, OUT cb NUMERIC, OUT bb NUMERIC, OUT ba NUMERIC, OUT aa NUMERIC)
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    totalEstudantes INT;
+    totalFail INT := 0;
+    totalDD INT := 0;
+    totalDC INT := 0;
+    totalCC INT := 0;
+    totalCB INT := 0;
+    totalBB INT := 0;
+    totalBA INT := 0;
+    totalAA INT := 0;
+    r record;
+BEGIN
+    SELECT COUNT(grade) INTO totalEstudantes FROM student_prediction WHERE gender = genero;
 
+    FOR r IN (SELECT grade FROM student_prediction WHERE gender = genero)
+    LOOP
+        CASE r.grade
+            WHEN 0 THEN totalFail := totalFail + 1;
+            WHEN 1 THEN totalDD := totalDD + 1;
+            WHEN 2 THEN totalDC := totalDC + 1;
+            WHEN 3 THEN totalCC := totalCC + 1;
+            WHEN 4 THEN totalCB := totalCB + 1;
+            WHEN 5 THEN totalBB := totalBB + 1;
+            WHEN 6 THEN totalBA := totalBA + 1;
+            WHEN 7 THEN totalAA := totalAA + 1;
+        END CASE;
+    END LOOP;
+
+    fail := ROUND((totalFail * 100.0) / totalEstudantes, 1);
+    dd := ROUND((totalDD * 100.0) / totalEstudantes, 1);
+    dc := ROUND((totalDC * 100.0) / totalEstudantes, 1);
+    cc := ROUND((totalCC * 100.0) / totalEstudantes, 1);
+    cb := ROUND((totalCB * 100.0) / totalEstudantes, 1);
+    bb := ROUND((totalBB * 100.0) / totalEstudantes, 1);
+    ba := ROUND((totalBA * 100.0) / totalEstudantes, 1);
+    aa := ROUND((totalAA * 100.0) / totalEstudantes, 1);
+    
+    RAISE NOTICE 'percentual de estudantes do genero % que obtiveram cada nota -> | Fail: % | DD: % | DC: % | CC: % | CB: % | BB: % | BA: % | AA: % |', genero, fail, dd, dc, cc, cb, bb, ba, aa;
+END;
+$$;
+
+DO $$
+DECLARE
+    fail NUMERIC;
+    dd NUMERIC;
+    dc NUMERIC;
+    cc NUMERIC;
+    cb NUMERIC;
+    bb NUMERIC;
+    ba NUMERIC;
+    aa NUMERIC;
+BEGIN
+	RAISE NOTICE 'genero 1 = feminino; genero 2 = masculino.';
+    CALL percentualNotasGenero(1, fail, dd, dc, cc, cb, bb, ba, aa);
+	CALL percentualNotasGenero(2, fail, dd, dc, cc, cb, bb, ba, aa);
+END;
+$$;
 
 -- 1.5 Escreva as seguintes functions (incluindo um bloco anônimo de teste para cada uma):
 
